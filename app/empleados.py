714a1876db84da639adb53aef74486537b6e0fcf
Pxnque <panque08@outlook.es>
2024-04-27 21:10:10 -0600

    created tabla 'trabajador',created empleados.py

---
+++ app/empleados.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
+
+window = tkinter.Tk()
+window.title("Login")
+window.geometry('1280x720')
+window.configure(bg='#C4C4C4')
+
+frame = tkinter.Frame(window)
+frame.pack()
+
+window.mainloop()
